ce211ba1139a8f9f3d1c9b6e5268b0308f4b7cc0
Dwayne Forde <dwayne@tenkeylabs.com>
2025-05-25 10:51:26 -0400

    chore: leaner builds that don't bundle external dependencies

---
+++ .changeset/hot-chairs-grin.md
@@ -0,0 +1,5 @@
+---
+"@withmantle/tracewright": patch
+---
+
+chore: leaner rollup build that doesn't bundle dependencies
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
-import babel from '@rollup/plugin-babel' // Transpiles modern JavaScript code
 import { nodeResolve } from '@rollup/plugin-node-resolve' // Allows Rollup to resolve modules from node_modules
 import commonjs from '@rollup/plugin-commonjs' // Converts CommonJS modules to ES6, allowing them to be included in the bundle.
 import terser from '@rollup/plugin-terser';
 import typescript from 'rollup-plugin-typescript2' // Compiles TypeScript files
-import copy from 'rollup-plugin-copy' // Copy essential files
 import path from 'path'; // Path declaration
 import json from '@rollup/plugin-json';
+
+const pkg = require('./package.json')
+
+const external = [
+  ...Object.keys(pkg.dependencies),
+  ...Object.keys(pkg.peerDependencies),
+]
 
 export default [
   {
     input: 'src/index.ts', // Entry point for CommonJS and ESM builds
+    treeshake: true,
+    external,
     output: [
       {
         dir: 'dist/cjs', // Output directory for CommonJS format
@@ -27,15 +34,13 @@ export default [
       }
     ],
     plugins: [
+      nodeResolve({
+        preferBuiltins: true,
+      }),
       json(),
       commonjs(),
       typescript({
         tsconfig: path.resolve(__dirname, './tsconfig.json'),
-      }),
-      babel({
-        exclude: 'node_modules/**/*',
-        babelHelpers: 'runtime',
-        plugins: ['@babel/plugin-transform-runtime']
       }),
       terser(),
     ]
